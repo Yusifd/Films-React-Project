36ab16b3a856815a8f7197952e0584f22c555f47
Yusifd <yusifdavidov@mail.ru>
2024-05-16 15:36:46 +0400

    FinalProject

--- films-project/src/components/Favorites/Favorites.jsx
+++ films-project/src/components/Favorites/Favorites.jsx
@@ -1,27 +1,24 @@
-import { useState,useEffect } from 'react';
-import './Favorites.css';
+import '../Favorites/Favorites.css'
 import {useDispatch, useSelector} from 'react-redux'
+import { deleteFromList } from "../../store/reducer";
+import { useEffect, useState } from 'react';
 import {Link} from 'react-router-dom'
-import { notClickedChange,deleteFromList } from '../../store/reducer';
-
-
-
-
+import { notClickedChange } from '../../store/reducer';
 function Favorites() {
-    let notClickedFavorites=useSelector(state=>state.movies.notClickedFavorites);
-    let dispatch=useDispatch()
-    let favorites=useSelector(state=>state.movies.favorites);
-    let [buttonText,setButtonText]=useState('Сохранить список');
-    let [id,setId]=useState('');
-    useEffect(()=>{
-        if(notClickedFavorites===true)
-        {
+    let notClickedFavorites = useSelector(state => state.movies.notClickedFavorites);
+    let dispatch = useDispatch()
+    let favorites = useSelector(state => state.movies.favorites);
+    let [buttonText, setButtonText] = useState('Сохранить список');
+    let [id, setId] = useState('');
+
+    useEffect(() => {
+        if (notClickedFavorites === true) {
             setButtonText('Сохранить список');
-        }
-        else{
+        } else {
             setButtonText('Изменить список');
         }
-    })
+    }, [notClickedFavorites]);
+
     const url = 'https://acb-api.algoritmika.org/api/movies/list/';
     const data = {
         "title": "mylist",
@@ -29,55 +26,55 @@ function Favorites() {
             ...favorites
         ]
     }
-    const handleSave=async()=>{
+
+    const handleSave = async () => {
         try {
             const response = await fetch(url, {
-            method: 'POST', 
-            body: JSON.stringify(data),
-            headers: {
-            'Content-Type': 'application/json'
-            }
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
             });
             const json = await response.json();
             setId(json.id);
         } catch (error) {
-        console.error('Ошибка:', error);
+            console.error('Ошибка:', error);
         }
     }
 
-    useEffect(()=>{
-        if(!notClickedFavorites){
+    useEffect(() => {
+        if (!notClickedFavorites) {
             handleSave();
-        }   
-    },[notClickedFavorites]);
+        }
+    }, [notClickedFavorites]);
 
     return (
         <div className="favorites">
-            <center>
-                <h1>Ваш список</h1>
-            <ul className="favorites__list">
-            {favorites.map((movie)=>{
-                        return(
+            {favorites.length === 0 ? <h1 className="favorites__empty-text">Ваш список пуст</h1> :
+                <ul className="favorites__list">
+                    {favorites.map((movie) => {
+                        return (
                             <li className="favorites__movies-item" key={movie.imdbID}>
-                                <img className="movies-item__poster" src={movie.Poster!='N/A' ? `${movie.Poster}` : `default-movie.png`} alt="poster"/>
+                                <img className="movies-item__poster" src={movie.Poster !== 'N/A' ? `${movie.Poster}` : `default-movie.png`} alt="poster" />
                                 <h3 className="movies-item__title">{movie.Title} ({movie.Year})</h3>
                                 {notClickedFavorites &&
-                                <button onClick={()=>{
-                                    dispatch(deleteFromList({
-                                        imdbID:movie.imdbID,
-                                    }))
-                                }} className="movies-item__delete-button">Удалить ❌</button>}
+                                    <button onClick={() => {
+                                        dispatch(deleteFromList({
+                                            imdbID: movie.imdbID,
+                                        }))
+                                    }} className="movies-item__delete-button">Удалить ❌</button>}
                             </li>
                         )
                     })}
-            </ul>
-            </center>
+                </ul>
+            }
             <center>
-            <button onClick={()=>{
-                    dispatch(notClickedChange());
-                    }} className="favorites__save">{buttonText}</button>
-                {!notClickedFavorites && <Link to={`/list/${id}`} className='favorites__watch-button'>Смотреть сейчас</Link>}
-                </center>
+            <button onClick={() => {
+                dispatch(notClickedChange());
+            }} className="favorites__save">{buttonText}</button>
+            {!notClickedFavorites && id && <Link to={`/list/${id}`} className='favorites__watch-button'>Смотреть сейчас</Link>}
+            </center>
         </div>
     );
 }
